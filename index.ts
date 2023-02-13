e0e2701c45ac2cb78f36d61c84307eb9d73670bd
supattana-s <supattana.sue@gmail.com>
2023-02-13 22:38:46 +0700

    change start script ts-node -> ts-node-dev

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const port = 8000;
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello from Express + TS (NOt Ts yet");
+    res.send("Hello from Express + TS!!!!!");
 });
 
 app.listen(port, () => console.log(`server listening on port: ${port}`));
