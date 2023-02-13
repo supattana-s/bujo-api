1f611ab7fcde7c95fd66d9abc61f82abc1de1003
supattana-s <supattana.sue@gmail.com>
2023-02-13 22:21:40 +0700

    install @type/express, @type/node

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+const port = 8000;
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello from Express + TS (NOt Ts yet");
+});
+
+app.listen(port, () => console.log(`server listening on port: ${port}`));
