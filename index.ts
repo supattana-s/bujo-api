7775189e4839f676e54b0f232a109772ec0389e3
supattana-s <supattana.sue@gmail.com>
2023-02-14 11:49:54 +0700

    install @types/cors, config for basic cors

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { Express, NextFunction, Request, Response } from "express";
+import express, {
+    Express,
+    NextFunction,
+    Request,
+    Response,
+    urlencoded,
+} from "express";
 import { Send, Query } from "express-serve-static-core";
+import cors from "cors";
 
 const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send({
